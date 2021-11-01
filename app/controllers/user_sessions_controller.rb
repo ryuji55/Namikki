ee68499804ed06b79b9073c36370495f8a6851c9
ryuji55 <rt.october.2@icloud.com>
2021-11-01 21:19:38 +0900

    Add:ユーザー登録・ログイン機能実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,15 +4,14 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to(:users, notice: 'Login successful')
+      redirect_back_or_to root_path
     else
-      flash.now[:alert] = 'Login failed'
-      render action: 'new'
+      render :new
     end
   end
 
   def destroy
     logout
-    redirect_to(:users, notice: 'Logged out!')
+    redirect_to login_path
   end
 end
