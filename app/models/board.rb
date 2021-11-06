f8a903d43909e987865b6a4f39a1348e7019146c
ryuji55 <rt.october.2@icloud.com>
2021-11-06 17:18:00 +0900

    Add:アソシエーションを定義

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,5 @@
 class Board < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites
 end
---
+++ spec/factories/favorites.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :favorite do
+    user { nil }
+    board { nil }
+  end
+end
