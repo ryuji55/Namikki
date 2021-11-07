634f0dcf137ace8d41aeb9e45754d1827fd4c1cc
ryuji55 <rt.october.2@icloud.com>
2021-11-07 18:02:53 +0900

    Add:投稿機能実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,12 +4,27 @@ class BoardsController < ApplicationController
     @faborite = Favorite.new
   end
 
-  def create
+  def new
     @board = Board.new
-    @board.user_id = current_user.id
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path, success: '投稿が完了しました'
+    else
+      flash.now['danger'] = '投稿に失敗しました'
+      render :new
+    end
   end
 
   def show
     @board = Board.find(params[:id])
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:point, :wave_size, :body)
   end
 end
