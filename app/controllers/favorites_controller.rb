601e915e196e0f58bfd8d4ccc009e9b77692cbc3
ryuji55 <rt.october.2@icloud.com>
2021-11-08 21:13:28 +0900

    Fix:コードのリファクタリング

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   before_action :set_board
   def create
-    @favorite = Favorite.create(board_id: params[:board_id], user_id: current_user.id)
+    @favorite = current_user.favorites.create(board_id: params[:board_id])
   end
 
   def destroy
-    @favorite = Favorite.find_by(board_id: params[:board_id], user_id: current_user.id)
+    @favorite = current_user.favorites.find_by(board_id: params[:board_id])
     @favorite.destroy
   end
 
