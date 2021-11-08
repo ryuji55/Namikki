944bf5decb2613fea1fa325dc711d4af69d9324e
ryuji55 <rt.october.2@icloud.com>
2021-11-08 14:33:55 +0900

    Add:一方通行の非同期処理まで

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,11 @@ class FavoritesController < ApplicationController
   before_action :set_board
   def create
     @favorite = Favorite.create(board_id: params[:board_id], user_id: current_user.id)
-    redirect_back(fallback_location: boards_path)
   end
 
   def destroy
     @favorite = Favorite.find_by(board_id: params[:board_id], user_id: current_user.id)
     @favorite.destroy
-    redirect_back(fallback_location: boards_path)
   end
 
    private
