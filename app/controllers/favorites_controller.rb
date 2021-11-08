62bf41a43825124bd163d11832b7563b117e6afb
ryuji55 <rt.october.2@icloud.com>
2021-11-08 11:21:17 +0900

    Add:いいねcreateの非同期処理完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,11 @@
 class FavoritesController < ApplicationController
 def create
-    board = Board.find(params[:board_id])
-    current_user.favorite(board)
-    redirect_back(fallback_location: root_path)
+    @board = Board.find(params[:board_id])
+    current_user.favorite(@board)
   end
 
   def destroy
     @favorite = Favorite.find_by(board_id: params[:board_id], user_id: current_user.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
