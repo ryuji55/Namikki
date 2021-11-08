01b4d30b6dd72adaa60ef26089a6bbca36275e39
ryuji55 <rt.october.2@icloud.com>
2021-11-08 11:43:06 +0900

    Fix:試行錯誤とboard_controllerタイポ修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,7 @@
 class BoardsController < ApplicationController
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc).page(params[:page])
-    @faborite = Favorite.new
+    @favorite = Favorite.new
   end
 
   def new
