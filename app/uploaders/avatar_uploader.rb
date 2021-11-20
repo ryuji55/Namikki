51dba7f4262d65e89f61c6305933f7a0052d2de3
ryuji55 <rt.october.2@icloud.com>
2021-11-21 01:19:18 +0900

    Add:S3を作成して本番環境の送信先として設定

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [300, 200]
 
-  storage :file
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
