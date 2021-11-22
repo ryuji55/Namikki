d9f1c1dd2f1e789f1300414a049c83a348dd7938
ryuji55 <rt.october.2@icloud.com>
2021-11-22 16:53:58 +0900

    Add:APIを叩いてTopページに表示させた

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,11 @@
 class StaticPagesController < ApplicationController
-  def top; end
+  def top
+    uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=Tahara&appid=#{Rails.application.credentials.weather[:api_key]}")
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+    result["weather"]
+    @main = result["weather"][0]["main"]
+    @description = result["weather"][0]["description"]
+    @icon = result["weather"][0]["icon"]
+  end
 end
