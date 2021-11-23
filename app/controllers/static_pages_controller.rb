19542fa247cfcc4d56172552476f0c773a027d39
ryuji55 <rt.october.2@icloud.com>
2021-11-24 00:49:08 +0900

    Add:3時間毎の天気予報を表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,12 @@
 class StaticPagesController < ApplicationController
   def top
-    uri = URI.parse("http://api.openweathermap.org/data/2.5/weather?q=Tahara&appid=#{Rails.application.credentials.weather[:api_key]}")
+    uri = URI.parse("https://api.openweathermap.org/data/2.5/forecast?id=1851259&lang=ja&appid=#{Rails.application.credentials.weather[:api_key]}")
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
-    result["weather"]
-    @main = result["weather"][0]["main"]
-    @description = result["weather"][0]["description"]
-    @icon = result["weather"][0]["icon"]
+    @lists = result["list"]
+   # @weather_main = result["list"][0]["weather"][0]["main"]
+   # @weather_description = result["list"][0]["weather"][0]["description"]
+   # @weather_icon = result["list"][0]["weather"][0]["icon"]
+   # @weather_date = result["list"][0]["dt_txt"]
   end
 end
