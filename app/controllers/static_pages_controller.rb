9506fd4c37ccd1d344c2dbd7feb0817233cc2163
ryuji55 <rt.october.2@icloud.com>
2021-11-24 10:34:35 +0900

    Add:気温表示をわかりやすく整えた

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,8 @@
 class StaticPagesController < ApplicationController
   def top
-    uri = URI.parse("https://api.openweathermap.org/data/2.5/forecast?id=1851259&lang=ja&appid=#{Rails.application.credentials.weather[:api_key]}")
+    uri = URI.parse("https://api.openweathermap.org/data/2.5/forecast?id=1851259&lang=ja&units=metric&appid=#{Rails.application.credentials.weather[:api_key]}")
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
     @lists = result["list"]
-   # @weather_main = result["list"][0]["weather"][0]["main"]
-   # @weather_description = result["list"][0]["weather"][0]["description"]
-   # @weather_icon = result["list"][0]["weather"][0]["icon"]
-   # @weather_date = result["list"][0]["dt_txt"]
   end
 end
