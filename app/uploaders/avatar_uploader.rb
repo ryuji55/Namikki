d191287aa6b52d3872ad84acea81ca6978c69d61
ryuji55 <rt.october.2@icloud.com>
2021-11-27 21:48:17 +0900

    Fix:S3に保存させるように設定した

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,13 +2,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [300, 200]
 
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
     storage :fog
-  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
