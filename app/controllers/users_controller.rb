c9163aa6a003052bf76125b3c73ff2bad5741399
ryuji55 <rt.october.2@icloud.com>
2021-11-30 22:29:54 +0900

    Add:エラーメッセージの表示調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to login_path, success: 'ユーザー登録が完了しました'
     else
-      flash.now[:danger] = 'ユーザー登録に失敗しました'
       render :new
     end
   end
