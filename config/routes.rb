ee68cc8a42e0ae2622f8f712fa4726cb4a2debb9
ryuji55 <rt.october.2@icloud.com>
2021-12-08 00:51:32 +0900

    Add:footerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   root 'static_pages#top'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'terms', to: 'static_pages#terms'
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
@@ -12,4 +15,5 @@ Rails.application.routes.draw do
   end
   resources :password_resets, only: %i[new create edit update]
   resource :profile, only: %i[show edit update destroy]
+  resources :contacts, only: %i[create new]
 end
