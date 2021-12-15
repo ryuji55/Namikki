a845b058da2094e72a700845ed470261b5767444
ryuji55 <rt.october.2@icloud.com>
2021-12-16 01:26:41 +0900

    Add:rails g model Notice

---
+++ db/migrate/20211215123107_create_notices.rb
@@ -0,0 +1,20 @@
+class CreateNotices < ActiveRecord::Migration[6.1]
+  def change
+    create_table :notices do |t|
+      t.integer :visitor_id, null: false
+      t.integer :visited_id, null: false
+      t.integer :board_id
+      t.integer :comment_id
+      t.integer :favorite_id
+      t.string :action, default: '', null: false
+      t.boolean :checked, default: false, null: false
+
+      t.timestamps
+    end
+    add_index :notice, :visitor_id
+    add_index :notice, :visited_id
+    add_index :notice, :board_id
+    add_index :notice, :comment_id
+    add_index :notice, :favorite_id
+  end
+end
