e46832d3efa2794b21bab51f0e854898f6385492
ryuji55 <rt.october.2@icloud.com>
2021-12-16 21:13:07 +0900

    Add:modelにメソッドを定義した

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,4 +5,43 @@ class Board < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
   has_many :comments, dependent: :destroy
+
+  def create_notice_favorite(current_user)
+    # すでに「いいね」されているか検索
+    temp = Notice.where(["visitor_id = ? and visited_id = ? and board_id = ? and action = ? ", current_user.id, user_id, id, 'favorite'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notice = current_user.active_notices.new(
+        board_id: id,
+        visited_id: user_id,
+        action: 'favorite'
+      )
+    end
+    # 自分の投稿に対するいいねの場合は、通知済みとする
+    notice.checkd = true if notice.visitor_id == notice.visited_id
+    notice.save if notice.valid?
+  end
+
+  def create_notice_comment!(current_user, comment_id)
+    # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
+    temp_ids = Comment.select(:user_id).where(board_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notice_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    # まだ誰もコメントしていない場合は、投稿者に通知を送る
+    save_notice_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notice_comment!(current_user, comment_id, visited_id)
+    # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
+    notice = current_user.active_notices.new(
+      board_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    # 自分の投稿に対するコメントの場合は、通知済みとする
+    notice.checked = true if notice.visitor_id == notice.visited_id
+    notice.save if notice.valid?
+  end
 end
