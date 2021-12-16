e25eb47b9684bdd5da2267fb8e01281e9d50b808
ryuji55 <rt.october.2@icloud.com>
2021-12-16 22:21:33 +0900

    Add:notices_controller実装

---
+++ app/controllers/notices_controller.rb
@@ -0,0 +1,8 @@
+class NoticesController < ApplicationController
+  def index
+    @notices = current_user.passive_notices.page(params[:page]).per(20)
+    @notices.where(checked: false).each do |notice|
+      notice.update(checked: true)
+    end
+  end
+end
