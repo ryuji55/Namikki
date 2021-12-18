346184a8d059c0a754422276747b1cd41fb70fcc
ryuji55 <rt.october.2@icloud.com>
2021-12-19 03:12:36 +0900

    Fix:ベルの大きさ調整・n+1問題修正

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,6 +1,6 @@
 class NoticesController < ApplicationController
   def index
-    @notices = current_user.passive_notices.includes({board: :user}, :visitor, :board).page(params[:page])
+    @notices = current_user.passive_notices.includes({board: :user}, :board).page(params[:page])
     @notices.where(checked: false).each do |notice|
       notice.update(checked: true)
     end
