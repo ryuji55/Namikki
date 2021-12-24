06a7f2a3c379ce7218d6d04d681d9788d75676ef
ryuji55 <rt.october.2@icloud.com>
2021-12-25 01:14:42 +0900

    Fix:Mypageと編集ページのレイアウト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,8 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :reset_password_token, uniqueness: true, allow_nil: true
-
   validates :email, uniqueness: true
+  validates :reason, length: { maximum: 150 }
 
   def own?(object)
     id == object.user_id
