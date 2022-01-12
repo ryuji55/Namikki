b10bfbe799d2fb209bf2f00c49b5c4b80daf2cb8
ryuji55 <rt.october.2@icloud.com>
2022-01-12 13:47:36 +0900

    Add:コメント名 ・アイコンから他者のものならユーザー詳細ページへ・自身のものならプロフィールページへ遷移

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :comment do
     body { "テストコメント" }
-    user_id { 1 }
-    board_id { 1 }
+    sequence(:user_id, "1")
+    sequence(:board_id, "1")
     association :user
     association :board
   end
